48035159574034a39e224683059064213750076e
Edmundo Duarte <edmundoduarte1998@gmail.com>
2025-05-28 22:23:17 -0300

    definição de cores nas notificações

--- app/src/main/java/com/example/pi3/coordenador/Actions/ExpiringActivitiesAdapter.kt
+++ app/src/main/java/com/example/pi3/coordenador/Actions/ExpiringActivitiesAdapter.kt
@@ -10,10 +10,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.pi3.R
 import com.example.pi3.data.ActivitieRepository
 import com.example.pi3.model.Activitie
-import java.text.SimpleDateFormat
-import java.util.Date
-import java.util.Locale
-import java.util.concurrent.TimeUnit
+
 
 class ExpiringActivitiesAdapter(
     private val activities: List<Activitie>,
@@ -26,24 +23,8 @@ class ExpiringActivitiesAdapter(
 
         fun bind(activity: Activitie) {
             txtActivityTitle.text = activity.titulo ?: "Sem título"
-            val daysRemaining = calculateDaysRemaining(activity.dataFim)
+            val daysRemaining = repository.calculateDaysRemaining(activity.dataFim)
             txtExpirationDays.text = "Expira em $daysRemaining dias"
-        }
-
-        private fun calculateDaysRemaining(dateString: String): Long {
-            val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
-            return try {
-                val endDate = dateFormat.parse(dateString)
-                val currentDate = Date()
-                if (endDate != null) {
-                    val diffInTime = endDate.time - currentDate.time
-                    TimeUnit.DAYS.convert(diffInTime, TimeUnit.MILLISECONDS)
-                } else {
-                    -1 // Indicate error or invalid date
-                }
-            } catch (e: Exception) {
-                -1 // Indicate error
-            }
         }
     }
 
